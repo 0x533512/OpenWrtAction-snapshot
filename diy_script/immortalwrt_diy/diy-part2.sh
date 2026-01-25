e6d2f717ad2f3e97b45451d38589729fad616dbf
David Mandy <smallprogram@foxmail.com>
2026-01-25 22:20:01 +0800

    packages: use openwrt snapshot source replace golang and rust

--- diy_script/immortalwrt_diy/diy-part2.sh
+++ diy_script/immortalwrt_diy/diy-part2.sh
@@ -19,18 +19,19 @@ sed -i 's/192.168.1.1/10.10.0.253/g' package/base-files/files/bin/config_generat
 rm -rf ./feeds/packages/net/open-app-filter
 
 
-# rm -rf temp_resp
-# git clone -b master --single-branch https://github.com/openwrt/packages.git temp_resp/openwrt_packages
+rm -rf temp_resp
+git clone -b master --single-branch https://github.com/openwrt/packages.git temp_resp/openwrt_packages
 # git clone -b main --single-branch https://github.com/openwrt/openwrt.git temp_resp/openwrt_source
 
-# # update golang version
-# rm -rf feeds/packages/lang/golang
-# cp -rf temp_resp/openwrt_packages/lang/golang feeds/packages/lang
-# rm -rf feeds/packages/lang/rust
-# cp -rf temp_resp/openwrt_packages/lang/rust feeds/packages/lang
+# update golang version
+rm -rf feeds/packages/lang/golang
+cp -rf temp_resp/openwrt_packages/lang/golang feeds/packages/lang
+rm -rf feeds/packages/lang/rust
+cp -rf temp_resp/openwrt_packages/lang/rust feeds/packages/lang
+
 # cp -rf temp_resp/openwrt_source/scripts/patch-kernel.sh scripts/
 
-# rm -rf temp_resp
+rm -rf temp_resp
 
 # golnag update version to 1.25.6
 # sed -i \
